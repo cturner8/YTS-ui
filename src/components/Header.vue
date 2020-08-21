1750c4a5ccb085375d1fd466457034c7498be9df
cturner8 <cammy_smfc@live.com>
2020-08-21 22:01:28 +0100

    initial conversion of flask based application, fetching from api

---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="header">
+    <nav class="navbar navbar-dark bg-dark">
+      <a class="navbar-brand">
+        <img src="../assets/youtube.svg" class="img-fluid" alt loading="lazy" />
+        YouTube Data Analysis
+      </a>
+      <span class="navbar-text">
+        <span v-if="has_filters">
+          <strong>Filters:</strong>
+          <span v-for="[key, value] in Object.entries(filter)" :key="key">
+            {{ key }} [
+            <strong>{{ value }}</strong>
+            ]
+          </span>
+        </span>
+        <a href="/upload" id="test" v-else
+          >Gain insight into your YouTube data</a
+        >
+      </span>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      has_filters: false,
+      filter: {},
+      name: "Cameron",
+    };
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,98 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Header />
+    <div class="row">
+      <div class="col contentDivs" v-if="ytData.channels">
+        <h2>Most popular channels</h2>
+        <ul class="list-group">
+          <li
+            class="list-group-item"
+            v-for="[key, value] in Object.entries(ytData.channels)"
+            :key="key"
+          >
+            <span class="leftAlign">{{ key }}</span>
+            <span class="rightAlign">
+              <strong>{{ value }}</strong>
+            </span>
+          </li>
+        </ul>
+      </div>
+      <div class="col contentDivs" v-if="ytData.raw_data">
+        <h2>Videos watched</h2>
+        <ul
+          class="list-group"
+          v-for="(item, index) in ytData.raw_data"
+          :key="index"
+        >
+          <li class="list-group-item" v-if="item.subtitles">
+            <span class="leftAlign">{{ item.title }}</span>
+            <span class="rightAlign">
+              <strong>{{ item.subtitles[0].name }}</strong>
+            </span>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Header from "../components/Header";
+import axios from "axios";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
-  }
+    Header,
+  },
+  data() {
+    return {
+      ytData: {
+        youtube_count: 0,
+        youtube_music_count: 0,
+        channels: [],
+        raw_data: [],
+      },
+    };
+  },
+  created() {
+    axios
+      .get("http://localhost:5000/search")
+      .then((res) => (this.ytData = { ...res.data.items }))
+      .catch((err) => console.log(err));
+  },
 };
 </script>
+
+<style>
+.home {
+  height: 100vh;
+}
+
+.mainContent {
+  height: 100%;
+}
+
+.leftDiv {
+  height: calc(100% - 7rem);
+  overflow-y: scroll;
+}
+
+.rightDiv {
+  height: calc(100% - 7rem);
+  overflow-y: scroll;
+}
+
+.contentDivs {
+  height: calc(100% - 7rem);
+  overflow-y: scroll;
+}
+
+.leftAlign {
+  float: left;
+}
+
+.rightAlign {
+  float: right;
+}
+</style>
