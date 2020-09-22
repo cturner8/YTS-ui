3b58de897038c18b62f0925bffdf4ea7d804365b
cturner8 <cammy_smfc@live.com>
2020-09-22 21:05:37 +0100

    initial form for passing in filter data

--- src/components/FileUploader.vue
+++ src/components/FileUploader.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="upload-file">
-    <b-container>
+  <div class="file-uploader flex-container column">
+    <b-container class="upload-file">
       <b-form-file
         class="file-selector"
         v-model="file"
@@ -11,9 +11,30 @@
       ></b-form-file>
       <div class="upload-buttons">
         <b-button @click="clearFiles" class="mr-2">Clear file</b-button>
-        <b-button :disabled="!file" @click="submitFiles(file)" class="mr-2" variant="success">Submit</b-button>
       </div>
       <div class="selected-file mt-3">Selected file: {{ file ? file.name : "" }}</div>
+    </b-container>
+    <b-container class="filter-form mt-3">
+      <b-form>
+        <b-form-group label="Title:" label-for="title">
+          <b-form-input id="title" v-model="filter.title" type="text" />
+        </b-form-group>
+
+        <b-form-group label="Date From:" label-for="dateFrom">
+          <b-form-input id="dateFrom" v-model="filter.dateFrom" type="text" />
+        </b-form-group>
+
+        <b-form-group label="Date To:" label-for="dateTo">
+          <b-form-input id="dateTo" v-model="filter.dateTo" type="text" />
+        </b-form-group>
+
+        <b-button
+          @click.prevent="submitFiles({file, filter})"
+          :disabled="!file"
+          type="button"
+          variant="danger"
+        >Submit</b-button>
+      </b-form>
     </b-container>
   </div>
 </template>
@@ -22,10 +43,11 @@
 import { mapActions } from "vuex";
 
 export default {
-  name: "Upload",
+  name: "FileUploader",
   data() {
     return {
-      file: null
+      file: null,
+      filter: {}
     };
   },
   methods: {
