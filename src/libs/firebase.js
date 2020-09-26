ee02babcaa568368afd4a0139e254b090181cdf1
cturner8 <cammy_smfc@live.com>
2020-09-26 19:52:51 +0100

    moving firebase config to seperate git ignored file

--- src/libs/firebase.js
+++ src/libs/firebase.js
@@ -1,15 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/auth";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDDlZ7mqfpRBxVM-eOqntxbBxnrjhdcp3Y",
-  authDomain: "yts-data-289517.firebaseapp.com",
-  databaseURL: "https://yts-data-289517.firebaseio.com",
-  projectId: "yts-data-289517",
-  storageBucket: "yts-data-289517.appspot.com",
-  messagingSenderId: "129272377295",
-  appId: "1:129272377295:web:b7a1e0990f518f53c9acba",
-};
+import { firebaseConfig } from "./config";
 
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
