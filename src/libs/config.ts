366b1e5b7ad8c20306ebae1207011e2e2d17f825
cturner8 <cammy_smfc@live.com>
2020-10-02 20:41:08 +0100

    removed config from git

--- src/libs/config.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDDlZ7mqfpRBxVM-eOqntxbBxnrjhdcp3Y",
-  authDomain: "yts-data-289517.firebaseapp.com",
-  databaseURL: "https://yts-data-289517.firebaseio.com",
-  projectId: "yts-data-289517",
-  storageBucket: "yts-data-289517.appspot.com",
-  messagingSenderId: "129272377295",
-  appId: "1:129272377295:web:e82ec53836a2e93cc9acba",
-};
--- src/libs/firebase.ts
+++ src/libs/firebase.ts
@@ -1,6 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/auth";
-import { firebaseConfig } from "./config";
+import { firebaseConfig } from "../../config";
 
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
