96218025fb7b510af6fc9e038d8615606fd65b0a
cturner8 <cammy_smfc@live.com>
2020-10-16 18:05:08 +0100

    setup for firebase deploy

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-  // This will be needed for GH Pages but most likely not Firebase
-  publicPath: process.env.NODE_ENV === "production" ? "/YTS-ui/" : "/",
+  publicPath: "/",
 };
